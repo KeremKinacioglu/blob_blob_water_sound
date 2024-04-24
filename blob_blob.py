beeb75661760e923be108b67f4ea84df5a2eb973
KeremKinacioglu <160053738+KeremKinacioglu@users.noreply.github.com>
2024-04-24 22:27:47 +0300

    Update blob_blob.py
    Yihaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- blob_blob.py
+++ blob_blob.py
@@ -1,11 +1,3 @@
-text = 'Hello World'
-shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-for i in text:
-    print (i)
-
-
 
 meme_dict = {
             "HMM": "Birçok anlamda kullanlıbilir genellikle anladım, öylemiymiş",
