2bdee6fa92354c8c528c21ea6908aa77552b7957
KeremKinacioglu <160053738+KeremKinacioglu@users.noreply.github.com>
2024-04-24 22:26:50 +0300

    Update blob_blob.py
    Yihaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- blob_blob.py
+++ blob_blob.py
@@ -4,3 +4,19 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 for i in text:
     print (i)
+
+
+
+meme_dict = {
+            "HMM": "Birçok anlamda kullanlıbilir genellikle anladım, öylemiymiş",
+            "EDİBLE": "EDİBLE kelimesini sınıfımdakiler dışında kimsenin kullanmadığına eminim ingilizcede anlamı Yenilebilir olsada bizdeki anlamı olabilir, neden olmasın,yapılabilir",
+            }
+        
+        
+        
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+    if word in meme_dict:
+        print(word, "kelimesinin anlamı =" , meme_dict[word])
+else:
+    print("Maalesef verdiğiniz kelime listemde yok ;(")
